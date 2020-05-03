073882613e83f7be4826e68a0bfc51b2315c318e
Marcus Sellmann <healingkahn@gmail.com>
2020-05-03 09:14:02 +0200

    Added console outputs to get more information of the ongoing process.

--- ImageRenamer.py
+++ ImageRenamer.py
@@ -7,11 +7,13 @@ if __name__ == "__main__":
         if len(sys.argv[1]) > 0:
             if os.path.exists(sys.argv[1]):
                 rootPath = sys.argv[1]
-
-    print('Scanning folder: ' + rootPath)
+                print('Searching in folder: ' + rootPath)
+            else:
+                print('The folder\n' + rootPath + '\n doesn\'t exist.')
 
     if len(rootPath) == 0:
         rootPath = os.path.abspath(os.getcwd())
+        print('Searching in folder: ' + rootPath)
 
     for root, dirs, files in os.walk(rootPath):
         for f in files:
@@ -27,5 +29,5 @@ if __name__ == "__main__":
                     creationTime = os.stat(srcPath).st_ctime
 
                 tgtPath = os.path.join(root, time.strftime('%Y%m%d_%H%M%S', time.gmtime(creationTime)) + fileExt)
-
+                print('Renaming: ' + f + ' -> ' + os.path.basename(tgtPath))
                 os.rename(srcPath, tgtPath)
